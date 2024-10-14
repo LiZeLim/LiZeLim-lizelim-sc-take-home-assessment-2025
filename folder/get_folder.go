2beb7f7a437a7cede640375424c6b71dd950354e
Li Ze Lim <lizelim995@gmail.com>
2024-10-14 19:29:00 +1100

    added implementation of component 1

--- folder/get_folder.go
+++ folder/get_folder.go
@@ -1,6 +1,12 @@
 package folder
 
-import "github.com/gofrs/uuid"
+import (
+	//"fmt"
+	"errors"
+	"sort"
+	"strings"
+	"github.com/gofrs/uuid"
+)
 
 func GetAllFolders() []Folder {
 	return GetSampleData()
@@ -17,11 +23,94 @@ func (f *driver) GetFoldersByOrgID(orgID uuid.UUID) []Folder {
 	}
 
 	return res
-
 }
 
-func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) []Folder {
-	// Your code here...
+/* Checks if the end of path matches the folder name */
+func ValidateFolderEndOfPath(folder Folder) bool {
+	pathLength := len(folder.Paths)
+	nameLength := len(folder.Name)
+	return folder.Paths[pathLength - nameLength:] == folder.Name
+}
 
-	return []Folder{}
+/* Validates if all previous folders up to the root folder have previously been seen */
+func validatePathStructure(path string, rootFoldername string, seen map[string]int) error {
+	splitPaths := strings.Split(path, ".")
+	if len(splitPaths) < 2 {
+		return errors.New("Error: invalid file path structure " + path)
+	}
+	for i := len(splitPaths) - 2; i >= 0; i-- { // all previous files must be seen in order for the current path to be valid
+		if _, exists := seen[splitPaths[i]]; exists {
+			if splitPaths[i] == rootFoldername {
+				break
+			}
+			continue
+		} else {
+			return errors.New("Error: path contains unseen folder " + path + " for " + splitPaths[i]) 
+		}
+	}
+	return nil
+}
+
+/* 
+	The main premise of the algorithm is to sort the list of folders by their path name.
+	By lexicographically sorting the paths, we can ensure that they are ordered correctly by their
+	hierarchy. 
+	*/
+func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) ([]Folder, error) {
+	if orgID.IsNil() {
+		return nil, errors.New("Error: Invalid orgID")
+	}
+
+	sameOriginFolders := f.GetFoldersByOrgID(orgID)
+
+	if len(sameOriginFolders) == 0 {
+		return nil, errors.New("Error: Folder does not exist in the specified organization")
+	}
+
+
+	/* sorting the folders of same origin to ensure child folders are in correct 
+	ordering, also allow for prev folder checking. */ 
+	sort.SliceStable(sameOriginFolders, func(i, j int) bool {
+		return sameOriginFolders[i].Paths < sameOriginFolders[j].Paths
+	})
+
+
+	var rootFolder *Folder
+	seen := make(map[string]int)
+
+	res := []Folder{}
+	for i := range sameOriginFolders {
+		f := &sameOriginFolders[i]
+
+		// finding root folder
+		if rootFolder == nil && f.Name == name {
+			rootFolder = f
+			seen[f.Name] = 1
+			continue // continue, root folder is not child folder
+		}
+
+		if rootFolder != nil { 
+			if len(f.Paths) > len(rootFolder.Paths) && f.Paths[:len(rootFolder.Paths)] == rootFolder.Paths {
+				if !ValidateFolderEndOfPath(*f) {
+					return nil, errors.New("Error: Folder name doesn't match end of path " + f.Paths)
+				}
+
+				err := validatePathStructure(f.Paths, rootFolder.Paths, seen)
+				if err != nil {
+					return nil, err
+				}
+
+				res = append(res, *f)
+				seen[f.Name] = 1
+			} else {
+				break //early exit since list is sorted, no other child will exist if previous conditions fail
+			}
+		}
+	}
+
+	if rootFolder == nil {
+		return nil, errors.New("Error: Folder does not exist")
+	}
+
+	return res, nil
 }
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	orgID := uuid.FromStringOrNil(folder.DefaultOrgID)
 
 	res := folder.GetAllFolders()
+	
 
 	// example usage
 	folderDriver := folder.NewDriver(res)
@@ -19,4 +20,18 @@ func main() {
 	folder.PrettyPrint(res)
 	fmt.Printf("\n Folders for orgID: %s", orgID)
 	folder.PrettyPrint(orgFolder)
+
+	// example usage of get all child folders
+	res = folder.GetSampleData()
+	folderDriver = folder.NewDriver(res)
+	orgID = uuid.FromStringOrNil("38b9879b-f73b-4b0e-b9d9-4fc4c23643a7")
+
+	rootFolderString := "clear-arclight" // subject to change
+	arcLightChildren, err := folderDriver.GetAllChildFolders(orgID, rootFolderString)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("\n\n Child folders of " + rootFolderString + ":")
+		folder.PrettyPrint(arcLightChildren)
+	}
 }
